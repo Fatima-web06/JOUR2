03b189def31f058ceadfe4d05457963acbc07c23
Fatima-web06 <fatima.fall@epitech.digital>
2024-10-01 15:50:49 +0200

    Create  ex_07.js

---
+++ ex_07.js
@@ -0,0 +1,13 @@
+ function getFruitNameFromIndex(index) { 
+  if (index === -1 || index === "Last") {
+    	return fruits[fruits.length -1];
+   } 
+   if (isNaN(index) || index <0 || index >= fruits.length) { 
+      return null;
+    } 
+    return fruits[index];	
+ }
+ 
+  let result = getFruitNameFromIndex("Last");
+  displayResult(result);
+      
