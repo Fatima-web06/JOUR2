5059340eb812422a54a511a4aaffd6f403b4ed71
Fatima-web06 <fatima.fall@epitech.digital>
2024-10-01 10:55:48 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,22 @@
+function changebackground() { 
+
+var magicNumber = 1;
+ if (magicNumber === 1) {
+
+ }   
+  paintRedRectangle();
+  if (magicNumber === 2) {
+ 	 let magicNumber = 2;
+  
+   
+  }
+  paintGreenRectangle();
+ if (magicNumber === 3) { 
+ 	let magicNumber = 3; 
+  
+
+ }      
+ paintYellowRectangle();
+}
+
+changebackground();
