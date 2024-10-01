bfc0ea9963e7af3d2066eec46909656b983d6230
Fatima-web06 <fatima.fall@epitech.digital>
2024-10-01 13:12:31 +0200

    Create  ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,29 @@
+function getBasketContent0() {
+let basket = [
+	'strawberry' , 'strawberry' ,'stawberry',
+  'apple', 'apple',
+  'lime', 'lime',
+  'peach',
+  'pear','pear',
+ ];
+ 
+ console.log(basket.length + "fruit(s) selected");
+ 
+ return basket;
+ }
+ 
+ getBasketContent();
+
+
+// DO NOT TOUCH, this will do the call your function for you and display the fruits if you've done it well
+if (typeof getBasketContent === "function") {
+	const fruits = getBasketContent()
+	showMyBasket(fruits)
+}ur function here ...
+
+
+// DO NOT TOUCH, this will do the call your function for you and display the fruits if you've done it well
+if (typeof getBasketContent === "function") {
+	const fruits = getBasketContent()
+	showMyBasket(fruits)
+}
