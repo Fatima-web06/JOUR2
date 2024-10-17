bf7ffb7c7a8de161c377f046c243814cb925eff1
Fatima-web06 <fatima.fall@epitech.digital>
2024-10-17 11:02:55 +0200

    Update ex_07.js

--- ex_07.js
+++ ex_07.js
@@ -1,4 +1,5 @@
- function getFruitNameFromIndex(index) { 
+ fruits = ["Blackberries", "Apple", "Banana", "Mango", "Peaches", "Kiwi"];
+function getFruitNameFromIndex(index) { 
   if (index === -1 || index === "Last") {
     	return fruits[fruits.length -1];
    } 
@@ -6,8 +7,6 @@
       return null;
     } 
     return fruits[index];	
- }
- 
+}
   let result = getFruitNameFromIndex("Last");
   displayResult(result);
-      
