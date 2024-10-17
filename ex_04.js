b695f7ff4f743768865b8fc4600fa74b7d98d156
Fatima-web06 <fatima.fall@epitech.digital>
2024-10-17 10:59:04 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,26 +1,26 @@
 let a = 2;
-  let b = 4;
-  let action = "sub";
+let b = 4
+let action = "sub";
 
-function addition() { 
-		return a + b;
-}
-
-function subtraction() { 
-		return a - b;
-}
-
-function calculator() {
-		if (action === "add") { 
-    		return addition();
-    } else if (action === "sub") {
-    		return subtraction();
-    } else { 
-    		return 0;
-    }
-    
+function addition() {
+return a + b ;
+ }
+ 
+function subtraction() {
+return a - b;
  }
 
+function calculator () {
+if (action === "add") {
+	return addition ();
+} else if (action === "sub") {
+  return subtraction ();
+ } else { 
+  return 0 ; 
+  }
+}
+ 
+  
 // DO NOT TOUCH - This line will call calculator and display the result for you
 if (typeof calculator === "function" && typeof a === "number" &&
 		typeof b === "number" && typeof action === "string") {
