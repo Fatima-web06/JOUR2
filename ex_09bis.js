bea316450a570b3da674408d71623ab5952f40ee
Fatima-web06 <fatima.fall@epitech.digital>
2024-10-17 11:06:12 +0200

    Update ex_09bis.js

--- ex_09bis.js
+++ ex_09bis.js
@@ -1,13 +1,45 @@
-function doOperation(operation) {
-		const[num1, operator, num2] = operation.split(' ');
-    const a = parseFloat(num1), b = parseFloat(num2);
+function splitOperation(operation) {
+    const parts = operation.split(" ");
+    const firstNumber = parseInt(parts[0], 10);
+    const operator = parts[1];
+    const secondNumber = parseInt(parts[2], 10);
+    return [firstNumber, operator, secondNumber];
+}
 
-    switch (operator) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': return b !== 0 ? a / b : (console.log("Can't divide by 0"), null);
-        default: return null;
+function addition(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    if (b === 0) {
+        console.log("Can't divide by 0");
+        return null;
+    }
+    return a / b;
+}
+
+function doOperation(operation) {
+
+    const [firstNumber, operator, secondNumber] = splitOperation(operation);
+   
+   switch (operator) {
+        case "+":
+            return addition(firstNumber, secondNumber);
+        case "-":
+            return subtraction(firstNumber, secondNumber);
+        case "*":
+            return multiplication(firstNumber, secondNumber);
+        case "/":
+            return division(firstNumber, secondNumber);
+        default:
+            return null; 
     }
 }
-    
