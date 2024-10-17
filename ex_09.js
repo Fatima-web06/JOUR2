f0ac0751e95649723a089eeacfd8c32b1abd5197
Fatima-web06 <fatima.fall@epitech.digital>
2024-10-17 11:05:02 +0200

    Update ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,8 +1,9 @@
-function splitOperation(operation) { 
-
-let parts = operation.split( '' );
-
-return [parseInt(parts[0]), parts [1], parseInt(parts[2])];
+function splitOperation(operation) {
+   
+    const parts = operation.split(" ");
+    const firstNumber = parseInt(parts[0], 10);
+    const operator = parts[1];
+    const secondNumber = parseInt(parts[2], 10);
+    
+    return [firstNumber, operator, secondNumber];
 }
-
-console.log(splitOperation("1 + 2"));
